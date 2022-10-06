c9c9d06b917ae22a6cf0e39d0c20d96c2e5a1cec
Adhiambo Peres <adhiambooyier@gmail.com>
2022-10-06 14:06:26 +0300

    determine the number of words per list that begins with each letter of the alphabet

--- Main.java
+++ Main.java
@@ -6,28 +6,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<String> inputList = new ArrayList<String>();
-        Scanner stdin = new Scanner(System.in);
 
-        System.out.println("this is the current input" + inputList.toString());
-        inputList.add(stdin.next());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        stdin.close();
+        try {
+            String[] line = br.readLine().split(",");
 
-        System.out.println("this is the input" + inputList);
+            for (int i = 0; i < line.length; i++) {
+                inputList.add(line[i].trim());
+            }
+            System.out.println(inputList);
+            getWordsPerLetter(inputList);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
-        // BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    }
 
-        // try{
-        // String[] line = br.readLine().split(",");
-        
-        // for(int i = 0; i<line.length; i++){
-        //     inputList.add(line[i]);
-        // }
-        // System.out.println("this is the input" + inputList);
-        // }catch(IOException ioe){
+    private static HashMap<Character, Integer> getWordsPerLetter(List<String> fruits) {
+        HashMap<Character, Integer> fruitsMap = new HashMap<>();
 
-        // }
+        for (char ch = 'A'; ch <= 'Z'; ++ch) {
+            fruitsMap.put(ch, 0);
+        }
 
-
+        for (String fruit : fruits) {
+            char fruitBeginsWith = fruit.charAt(1);
+            int currentValue = fruitsMap.get(fruitBeginsWith);
+            fruitsMap.replace(fruitBeginsWith, currentValue+1);
+        }
+        return fruitsMap;
     }
 }
