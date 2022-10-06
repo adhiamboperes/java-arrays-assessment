fb3853928d034545a30d7dc5fe8511f1f3a462fd
Adhiambo Peres <adhiambooyier@gmail.com>
2022-10-06 18:49:14 +0300

    Add choices to show output with list of words

--- Main.java
+++ Main.java
@@ -4,30 +4,55 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         List<String> inputList = new ArrayList<String>();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
+            System.out.println(
+                    "Select an output format\n Enter 1 to print number of words per letter\n Enter 2 to see list of words per letter");
+            int outputChoice = Integer.parseInt(br.readLine());
+
+            System.out.println("Enter your string array input:");
+
             String[] line = br.readLine().split(",");
 
             for (int i = 0; i < line.length; i++) {
                 inputList.add(line[i].trim());
             }
-            System.out.println(inputList);
-            printWordsPerAlphabet(inputList);
+
+            if (outputChoice == 1) {
+                printNumberOfWordsPerLetter(inputList);
+            } else {
+                printWordsAndNumberPerLetter(inputList);
+            }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
 
     }
 
-    private static void printWordsPerAlphabet(List<String> fruits){
+    private static void printNumberOfWordsPerLetter(List<String> fruits) {
         HashMap<Character, Integer> fruitsMap = getWordsPerLetter(fruits);
         fruitsMap.entrySet().forEach(entry -> {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         });
+    }
+
+    private static void printWordsAndNumberPerLetter(List<String> fruits) {
+        HashMap<Character, List<String>> multiMap = storeWordsPerLetter(fruits);
+        for (char ch = 'A'; ch <= 'Z'; ++ch) {
+            if (multiMap.containsKey(ch)) {
+                System.out.println(ch + ": " + multiMap.get(ch).size());
+                multiMap.get(ch).forEach(fruitInList -> {
+                    System.out.println(1 + " " + fruitInList.replace("\"", ""));
+                });
+            } else {
+                System.out.println(ch + ": " + 0);
+            }
+        }
     }
 
     private static HashMap<Character, Integer> getWordsPerLetter(List<String> fruits) {
@@ -40,8 +65,25 @@ public class Main {
         for (String fruit : fruits) {
             char fruitBeginsWith = fruit.charAt(1);
             int currentValue = fruitsMap.get(fruitBeginsWith);
-            fruitsMap.replace(fruitBeginsWith, currentValue+1);
+            fruitsMap.replace(fruitBeginsWith, currentValue + 1);
         }
         return fruitsMap;
+    }
+
+    private static HashMap<Character, List<String>> storeWordsPerLetter(List<String> fruits) {
+        HashMap<Character, List<String>> multiMap = new HashMap<>();
+
+        for (String fruit : fruits) {
+            char firstLetter = fruit.charAt(1);
+
+            if (multiMap.containsKey(firstLetter)) {
+                multiMap.get(firstLetter).add(fruit);
+            } else {
+                List<String> alphabetFruitsList = new ArrayList<>();
+                alphabetFruitsList.add(fruit);
+                multiMap.put(firstLetter, alphabetFruitsList);
+            }
+        }
+        return multiMap;
     }
 }
